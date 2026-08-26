2982fe0a738e2e1e9c9c06819420d7383fb54a57
Sebby Apudo <sebbyapudo@gmail.com>
2026-08-26 21:48:40 +0300

    Fixed profile issues

--- routes/settings.php
+++ routes/settings.php
@@ -15,8 +15,6 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
 
     Route::get('settings/security', [SecurityController::class, 'edit'])
         ->name('security.edit');
